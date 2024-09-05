0831df85160536039669f4e9a51e72a00fd9d24f
pgd <rudeh1253@gmail.com>
2024-09-05 16:24:41 +0900

    Feat: 회원 등록 Endpoint에 Security 설정
    Related to: #9

--- src/main/java/com/hf/healthfriend/domain/member/constant/Role.java
+++ src/main/java/com/hf/healthfriend/domain/member/constant/Role.java
@@ -1,7 +1,17 @@
 package com.hf.healthfriend.domain.member.constant;
 
 public enum Role {
-    ROLE_ADMIN,
-    ROLE_MEMBER,
-    ROLE_NON_MEMBER
+    ROLE_ADMIN("ADMIN"),
+    ROLE_MEMBER("MEMBER"),
+    ROLE_NON_MEMBER("NON_MEMBER");
+
+    private final String roleName;
+
+    Role(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public String roleName() {
+        return this.roleName;
+    }
 }
