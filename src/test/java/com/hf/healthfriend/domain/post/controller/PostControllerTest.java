aad4d1ba53895066c5131db869f0f836b9e49ad2
이가연 <99721126+dlrkdus@users.noreply.github.com>
2024-09-25 12:28:36 +0900

    feat: 페이징 테스트

---
+++ src/test/java/com/hf/healthfriend/domain/post/controller/PostControllerTest.java
@@ -0,0 +1,22 @@
+package com.hf.healthfriend.domain.post.controller;
+
+import org.junit.jupiter.api.Test;
+
+class PostControllerTest {
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
