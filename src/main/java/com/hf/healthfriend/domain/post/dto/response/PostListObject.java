50e89a3221f7fe904c8f7f59d54e028e2ad74d7d
이가연 <99721126+dlrkdus@users.noreply.github.com>
2024-09-28 00:57:10 +0900

    feat: 시간 형태 변경
    "yyyy-MM-dd HH:mm" -> N시간 전, N분 전, N초 전

--- src/main/java/com/hf/healthfriend/domain/post/dto/response/PostListObject.java
+++ src/main/java/com/hf/healthfriend/domain/post/dto/response/PostListObject.java
@@ -2,6 +2,7 @@ package com.hf.healthfriend.domain.post.dto.response;
 
 import lombok.Builder;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 @Builder
@@ -10,10 +11,24 @@ public record PostListObject(
         String category,
         String title,
         String content,
-        LocalDateTime creationTime,
+        String creationTime,
         long viewCount,
         long likeCount,
         long commentCount,
         String fitnessLevel
 ) {
+    public static String calculateTimeAgo(LocalDateTime creationTime) {
+        LocalDateTime now = LocalDateTime.now();
+        Duration duration = Duration.between(creationTime, now);
+
+        if (duration.toDays() > 0) {
+            return duration.toDays() + "일 전";
+        } else if (duration.toHours() > 0) {
+            return duration.toHours() + "시간 전";
+        } else if (duration.toMinutes() > 0) {
+            return duration.toMinutes() + "분 전";
+        } else {
+            return duration.toSeconds() + "초 전";
+        }
+    }
 }
