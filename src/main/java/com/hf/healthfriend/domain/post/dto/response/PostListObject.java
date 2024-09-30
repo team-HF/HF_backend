5a15091a013f2be145765ccb6c5b9f1af0e03ad8
이가연 <99721126+dlrkdus@users.noreply.github.com>
2024-09-30 15:34:07 +0900

    fix: 날짜 형식 변경

--- src/main/java/com/hf/healthfriend/domain/post/dto/response/PostListObject.java
+++ src/main/java/com/hf/healthfriend/domain/post/dto/response/PostListObject.java
@@ -17,18 +17,4 @@ public record PostListObject(
         long commentCount,
         String fitnessLevel
 ) {
-    public static String calculateTimeAgo(LocalDateTime creationTime) {
-        LocalDateTime now = LocalDateTime.now();
-        Duration duration = Duration.between(creationTime, now);
-
-        if (duration.toDays() > 0) {
-            return duration.toDays() + "일 전";
-        } else if (duration.toHours() > 0) {
-            return duration.toHours() + "시간 전";
-        } else if (duration.toMinutes() > 0) {
-            return duration.toMinutes() + "분 전";
-        } else {
-            return duration.toSeconds() + "초 전";
-        }
-    }
 }
