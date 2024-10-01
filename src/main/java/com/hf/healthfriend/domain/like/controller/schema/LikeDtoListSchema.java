0326b68d270e7b02400a8bf3e722bbfe47bce746
이가연 <99721126+dlrkdus@users.noreply.github.com>
2024-10-01 17:34:28 +0900

    feat: CommentLikeDto 추가 및 LikeDto-> PostLikeDto

--- src/main/java/com/hf/healthfriend/domain/like/controller/schema/LikeDtoListSchema.java
+++ src/main/java/com/hf/healthfriend/domain/like/controller/schema/LikeDtoListSchema.java
@@ -1,9 +1,9 @@
 package com.hf.healthfriend.domain.like.controller.schema;
 
-import com.hf.healthfriend.domain.like.dto.LikeDto;
+import com.hf.healthfriend.domain.like.dto.PostLikeDto;
 import com.hf.healthfriend.global.spec.ApiBasicResponse;
 
 import java.util.List;
 
-public class LikeDtoListSchema extends ApiBasicResponse<List<LikeDto>> {
+public class LikeDtoListSchema extends ApiBasicResponse<List<PostLikeDto>> {
 }
