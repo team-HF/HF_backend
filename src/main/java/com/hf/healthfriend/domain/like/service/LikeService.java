23114e8d18218b19a16086bb51f7c53825325835
이가연 <99721126+dlrkdus@users.noreply.github.com>
2024-10-01 18:30:37 +0900

    feat: 댓글 좋아요 API
    - 댓글에 좋아요 추가
    - 댓글에 달린 좋아요 목록 조회

--- src/main/java/com/hf/healthfriend/domain/like/service/LikeService.java
+++ src/main/java/com/hf/healthfriend/domain/like/service/LikeService.java
@@ -1,8 +1,13 @@
 package com.hf.healthfriend.domain.like.service;
 
-import com.hf.healthfriend.domain.like.dto.LikeDto;
+import com.hf.healthfriend.domain.comment.entity.Comment;
+import com.hf.healthfriend.domain.like.constant.LikeType;
+import com.hf.healthfriend.domain.like.dto.CommentLikeDto;
+import com.hf.healthfriend.domain.like.dto.PostLikeDto;
 import com.hf.healthfriend.domain.like.entity.Like;
-import com.hf.healthfriend.domain.like.exception.DuplicateLikeException;
+import com.hf.healthfriend.domain.like.exception.CommentOrMemberNotExistsException;
+import com.hf.healthfriend.domain.like.exception.DuplicateCommentLikeException;
+import com.hf.healthfriend.domain.like.exception.DuplicatePostLikeException;
 import com.hf.healthfriend.domain.like.exception.PostOrMemberNotExistsException;
 import com.hf.healthfriend.domain.like.repository.LikeRepository;
 import com.hf.healthfriend.domain.member.entity.Member;
@@ -28,17 +33,18 @@ public class LikeService {
      * @param memberId 좋아요를 남기는 회원의 ID. Not null
      * @param postId   좋아요를 남길 글의 ID. Not Null
      * @return 자동 생성된 Like entity의 ID
-     * @throws DuplicateLikeException DuplicateLikeException 특정 회원이 특정 포스트에 대해 이미 좋아요를 남겼는데 중복해서
+     * @throws DuplicatePostLikeException DuplicateLikeException 특정 회원이 특정 포스트에 대해 이미 좋아요를 남겼는데 중복해서
      *                                *                                좋아요를 남기려고 할 경우
      */
-    public Long addLike(Long memberId, Long postId) throws DuplicateLikeException {
+    public Long addPostLike(Long memberId, Long postId) throws DuplicatePostLikeException {
         Optional<Like> likeOp = this.likeRepository.findByMemberIdAndPostId(memberId, postId);
         if (likeOp.isEmpty()) {
             Like like = new Like(
                     new Member(memberId),
                     Post.builder()
                             .postId(postId)
-                            .build()
+                            .build(),
+                    LikeType.POST
             );
             try {
                 Like savedLike = this.likeRepository.save(like);
@@ -51,7 +57,36 @@ public class LikeService {
         Like like = likeOp.get();
 
         if (!like.isCanceled()) {
-            throw new DuplicateLikeException(postId, memberId);
+            throw new DuplicatePostLikeException(postId, memberId);
+        }
+
+        like.uncancel();
+
+        return like.getLikeId();
+    }
+
+    public Long addCommentLike(Long memberId, Long commentId) throws DuplicatePostLikeException {
+        Optional<Like> likeOp = this.likeRepository.findByMemberIdAndCommentId(memberId, commentId);
+        if (likeOp.isEmpty()) {
+            Like like = new Like(
+                    new Member(memberId),
+                    Comment.builder()
+                            .commentId(commentId)
+                            .build(),
+                    LikeType.COMMENT
+            );
+            try {
+                Like savedLike = this.likeRepository.save(like);
+                return savedLike.getLikeId();
+            } catch (DataIntegrityViolationException e) {
+                throw new CommentOrMemberNotExistsException(e, memberId, commentId);
+            }
+        }
+
+        Like like = likeOp.get();
+
+        if (!like.isCanceled()) {
+            throw new DuplicateCommentLikeException(commentId, memberId);
         }
 
         like.uncancel();
@@ -67,10 +102,10 @@ public class LikeService {
      * @return Like entity의 정보를 가진 Like entity
      * @throws NoSuchElementException likeId를 ID로 하는 Like entity가 없을 경우
      */
-    public LikeDto getLike(Long likeId) throws NoSuchElementException {
+    public PostLikeDto getLike(Long likeId) throws NoSuchElementException {
         Like searchedLike = this.likeRepository.findById(likeId).orElseThrow(() ->
                 new NoSuchElementException(String.format("존재하지 않는 Like입니다. ID=%d", likeId)));// TODO 하드코딩 고치기
-        return LikeDto.of(searchedLike);
+        return PostLikeDto.of(searchedLike);
     }
 
     /**
@@ -84,20 +119,32 @@ public class LikeService {
         return this.likeRepository.existsByMemberIdAndPostId(memberId, postId);
     }
 
-    public List<LikeDto> getLikeOfPost(Long postId) {
-        return entityListToDtoList(this.likeRepository.findByPostId(postId));
+    public List<PostLikeDto> getLikeOfPost(Long postId) {
+        return postEntityListToDtoList(this.likeRepository.findByPostId(postId));
+    }
+
+    public List<CommentLikeDto> getLikeOfComment(Long commentId) {
+        return commentEntityListToDtoList(this.likeRepository.findByCommentId(commentId));
     }
 
     public Long getLikeCountOfPost(Long postId) {
         return this.likeRepository.countByPostId(postId);
     }
 
-    public List<LikeDto> getLikeOfMember(Long memberId) {
-        return entityListToDtoList(this.likeRepository.findByMemberId(memberId));
+    public Long getLikeCountOfComment(Long commentId) {
+        return this.likeRepository.countByCommentId(commentId);
     }
 
-    private List<LikeDto> entityListToDtoList(List<Like> entities) {
-        return entities.stream().map(LikeDto::of).toList();
+    public List<PostLikeDto> getPostLikeOfMember(Long memberId, Long postId) {
+        return postEntityListToDtoList(this.likeRepository.findPostLikeByMemberId(memberId,postId));
+    }
+
+    private List<PostLikeDto> postEntityListToDtoList(List<Like> entities) {
+        return entities.stream().map(PostLikeDto::of).toList();
+    }
+
+    private List<CommentLikeDto> commentEntityListToDtoList(List<Like> entities) {
+        return entities.stream().map(CommentLikeDto::of).toList();
     }
 
     public void cancelLike(Long likeIdToCancel) throws NoSuchElementException {
