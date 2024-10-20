eb70b4f0dc8af8dd99a459499168a80e2bf0a617
이가연 <99721126+dlrkdus@users.noreply.github.com>
2024-10-20 15:48:36 +0900

    fix: redis url 분리

--- src/main/java/com/hf/healthfriend/global/config/RedisConfig.java
+++ src/main/java/com/hf/healthfriend/global/config/RedisConfig.java
@@ -4,16 +4,21 @@ import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.client.codec.StringCodec;
 import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
 @Configuration
 public class RedisConfig {
+
+    @Value("${redis.host}")
+    private String redisHost;
+
     @Bean
     public RedissonClient redissonClient() {
         Config config = new Config();
-        config.useSingleServer().setAddress("redis://localhost:6379");
+        config.useSingleServer().setAddress(redisHost);
         config.setCodec(new StringCodec());
         return Redisson.create(config);
     }
