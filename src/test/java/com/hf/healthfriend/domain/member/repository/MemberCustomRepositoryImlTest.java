b4c499bda16c71416978fa96a64c692ea0f92ceb
이가연 <99721126+dlrkdus@users.noreply.github.com>
2024-10-24 18:58:08 +0900

    test: 멤버 프로필 검색 test

--- src/test/java/com/hf/healthfriend/domain/member/repository/MemberCustomRepositoryImlTest.java
+++ src/test/java/com/hf/healthfriend/domain/member/repository/MemberCustomRepositoryImlTest.java
@@ -1,19 +1,24 @@
 package com.hf.healthfriend.domain.member.repository;
 
+import com.hf.healthfriend.domain.follow.entity.Follow;
+import com.hf.healthfriend.domain.follow.repository.FollowRepository;
 import com.hf.healthfriend.domain.member.constant.*;
 import com.hf.healthfriend.domain.member.dto.request.MembersRecommendRequest;
+import com.hf.healthfriend.domain.member.dto.response.MemberSearchResponse;
+import com.hf.healthfriend.domain.member.entity.Member;
 import com.hf.healthfriend.domain.member.repository.querydsl.MemberCustomRepositoryImpl;
 import com.hf.healthfriend.testutil.TestConfig;
 import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.Predicate;
-import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import org.junit.jupiter.api.*;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
@@ -22,22 +27,27 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
-@ExtendWith(MockitoExtension.class)
 @ActiveProfiles("test")
 @Import(TestConfig.class)
+@DataJpaTest
 public class MemberCustomRepositoryImlTest {
 
-    @Mock
-    private JPAQueryFactory queryFactory;
-
-    @InjectMocks
+    @Autowired
     private MemberCustomRepositoryImpl memberCustomRepository;
+
+    @Autowired
+    private MemberJpaRepository memberRepository;
+
+    @Autowired
+    private FollowRepository followRepository;
+
+    @Autowired
+    private TestEntityManager entityManager;
 
     private MembersRecommendRequest request;
 
     @BeforeEach
     public void setUp(){
-        MockitoAnnotations.openMocks(this);
         request = MembersRecommendRequest.builder()
                 .companionStyleList(List.of(CompanionStyle.GROUP))
                 .fitnessKindList(List.of(FitnessKind.FUNCTIONAL))
@@ -45,6 +55,38 @@ public class MemberCustomRepositoryImlTest {
                 .fitnessEagernessList(null)
                 .memberSortType(MemberSortType.MATCHING_COUNT)
                 .build();
+        List<Member> members = new ArrayList<>();
+        for (int i = 1; i <= 20; i++) {
+            Member member = Member.builder()
+                    .loginId("virtualUser"+i)
+                    .role(Role.ROLE_MEMBER)
+                    .name("Virtual User"+ i)
+                    .email("virtual@user.com"+i)
+                    .password("password")
+                    .nickname("VirtualNickname"+(char) ('A'+i))
+                    .city("Seoul")
+                    .district("Gangnam")
+                    .birthDate(LocalDate.of(1990, 1, 1))
+                    .gender(Gender.MALE)
+                    .introduction("This is a virtual member"+ i)
+                    .fitnessLevel(FitnessLevel.BEGINNER)
+                    .companionStyle(CompanionStyle.GROUP)
+                    .fitnessEagerness(FitnessEagerness.EAGER)
+                    .fitnessObjective(FitnessObjective.BULK_UP)
+                    .fitnessKind(FitnessKind.HIGH_STRESS)
+                    .build();
+            memberRepository.save(member);
+            members.add(member);
+        }
+        for(int i = 0; i<=18; i++){
+            Follow follow = Follow.builder()
+                    .followee(members.get(i))
+                    .follower(members.get(i+1))
+                    .build();
+            followRepository.save(follow);
+        }
+        entityManager.flush();
+        entityManager.clear();
     }
 
     @Test
@@ -67,6 +109,23 @@ public class MemberCustomRepositoryImlTest {
         assertThat(predicate.toString()).contains("member1.fitnessKind = FUNCTIONAL");
         assertThat(predicate.toString()).contains("member1.fitnessObjective = RUNNING");
         assertThat(predicate.toString()).doesNotContain("member1.fitnessEagerness = EAGER");
+    }
+
+    @Test
+    @DisplayName("profile search test")
+    public void profileSearchTest() {
+        //Given
+        String keyword = "VirtualNicknameB";
+        Pageable pageable = PageRequest.of(0, 10);
+
+        //When
+        List<MemberSearchResponse> searchedProfileList = memberCustomRepository.searchMembers(keyword, pageable);
+
+        //Then
+        assertEquals(1, searchedProfileList.size());
+        assertEquals("This is a virtual member1",searchedProfileList.get(0).getIntroduction());
+        assertEquals(1,searchedProfileList.get(0).getFollowerCount());
+
     }
 
 }
