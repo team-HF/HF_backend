b33881ab630dc037db8e063aae3b5a526b7c9120
pgd <rudeh1253@gmail.com>
2024-11-13 15:29:55 +0900

    feat: 쿠폰을 사용한 매칭 시 발생할 수 있는 예외 핸들링
    Related to: #113

--- src/main/java/com/hf/healthfriend/domain/matching/exception/MatchingErrorCode.java
+++ src/main/java/com/hf/healthfriend/domain/matching/exception/MatchingErrorCode.java
@@ -5,7 +5,9 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum MatchingErrorCode {
     MEMBER_NOT_FOUND(40401, "MAT001", "해당 회원이 존재하지 않습니다"),
-    OUT_OF_LIMIT_MATCHING_REQUEST(40001, "MAT002", "중복된 매칭이 있습니다");
+    OUT_OF_LIMIT_MATCHING_REQUEST(40001, "MAT002", "중복된 매칭이 있습니다"),
+    INVALID_COUPON_USAGE(40002, "MAT003", "사용할 수 없는 쿠폰입니다"),
+    COUPON_NOT_FOUND(40402, "MAT004", "쿠폰이 존재하지 않습니다");
 
     private final int status;
     private final String code;
--- src/main/java/com/hf/healthfriend/domain/matching/exception/MatchingExceptionHandlingControllerAdvice.java
+++ src/main/java/com/hf/healthfriend/domain/matching/exception/MatchingExceptionHandlingControllerAdvice.java
@@ -1,38 +1,53 @@
 package com.hf.healthfriend.domain.matching.exception;
 
+import com.hf.healthfriend.domain.coupon.exception.CouponNotFoundException;
+import com.hf.healthfriend.domain.coupon.exception.UnavailableCouponException;
 import com.hf.healthfriend.domain.member.exception.MemberNotFoundException;
 import com.hf.healthfriend.global.spec.BasicErrorResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import static com.hf.healthfriend.domain.matching.exception.MatchingErrorCode.*;
+
+@Slf4j
 @RestControllerAdvice(basePackages = "com.hf.healthfriend.domain.matching")
 public class MatchingExceptionHandlingControllerAdvice {
 
     @ExceptionHandler(MemberNotFoundException.class)
     public ResponseEntity<BasicErrorResponse> memberNotFoundException(MemberNotFoundException e) {
-        return new ResponseEntity<>(
-                from(MatchingErrorCode.MEMBER_NOT_FOUND),
-                HttpStatus.NOT_FOUND
-        );
+        return from(MEMBER_NOT_FOUND, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(OutOfLimitMatchingRequestException.class)
     public ResponseEntity<BasicErrorResponse> outOfLimitMatchingRequestException(OutOfLimitMatchingRequestException e) {
-        return new ResponseEntity<>(
-                from(MatchingErrorCode.OUT_OF_LIMIT_MATCHING_REQUEST),
-                HttpStatus.BAD_REQUEST
-        );
+        return from(OUT_OF_LIMIT_MATCHING_REQUEST, HttpStatus.BAD_REQUEST);
     }
 
-    private BasicErrorResponse from(MatchingErrorCode errorCode) {
-        return BasicErrorResponse.builder()
-                .statusCode(errorCode.status())
-                .errorName(errorCode.name())
-                .message(errorCode.message())
-                .statusCodeSeries(4)
-                .errorCode(errorCode.code())
-                .build();
+    @ExceptionHandler(CouponNotFoundException.class)
+    public ResponseEntity<BasicErrorResponse> couponNotFoundException(CouponNotFoundException e) {
+        log.info("존재하지 않는 쿠폰 사용 - couponId={}", e.getCouponId());
+        return from(COUPON_NOT_FOUND, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(UnavailableCouponException.class)
+    public ResponseEntity<BasicErrorResponse> unavailableCouponException(UnavailableCouponException e) {
+        log.info("유효하지 않은 쿠폰 사용 - couponId={}", e.getCouponId());
+        return from(INVALID_COUPON_USAGE, HttpStatus.BAD_REQUEST);
+    }
+
+    private ResponseEntity<BasicErrorResponse> from(MatchingErrorCode errorCode, HttpStatus status) {
+        return new ResponseEntity<>(
+                BasicErrorResponse.builder()
+                        .statusCode(errorCode.status())
+                        .errorName(errorCode.name())
+                        .message(errorCode.message())
+                        .statusCodeSeries(4)
+                        .errorCode(errorCode.code())
+                        .build(),
+                status
+        );
     }
 }
