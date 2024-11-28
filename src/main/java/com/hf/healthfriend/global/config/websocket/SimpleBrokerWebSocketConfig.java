54e154a74fee8d574ba597c6af4d21fe521bcd38
pgd <rudeh1253@gmail.com>
2024-11-28 14:14:31 +0900

    chore: 웹 소켓 관련 Configuration 클래스 구현
    Related to: #125

---
+++ src/main/java/com/hf/healthfriend/global/config/websocket/SimpleBrokerWebSocketConfig.java
@@ -0,0 +1,27 @@
+package com.hf.healthfriend.global.config.websocket;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class SimpleBrokerWebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/hf").withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/app");
+
+        // 내장 메시지 브로커
+        registry.enableSimpleBroker("/topic", "/queue", "/user");
+
+        registry.setUserDestinationPrefix("/user");
+    }
+}
