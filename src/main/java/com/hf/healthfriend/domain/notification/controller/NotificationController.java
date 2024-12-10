758931b34c7aacccff3502ad8207eaaa5c16f8eb
이가연 <99721126+dlrkdus@users.noreply.github.com>
2024-12-10 21:17:42 +0900

    feat: SSE 알람 전송

---
+++ src/main/java/com/hf/healthfriend/domain/notification/controller/NotificationController.java
@@ -0,0 +1,25 @@
+package com.hf.healthfriend.domain.notification.controller;
+
+import com.hf.healthfriend.domain.notification.service.NotificationSSEService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class NotificationController {
+
+    private final NotificationSSEService notificationSseService;
+
+    @GetMapping(value = "/connect/sse", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public ResponseEntity<SseEmitter> connect(
+            @RequestParam Long memberId,
+            @RequestHeader(value = "Last-Event-ID", required = false, defaultValue = "") String lastEventId) {
+        return ResponseEntity.ok( notificationSseService.connectClient(memberId,lastEventId));
+    }
+}
