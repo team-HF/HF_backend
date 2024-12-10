a88a91f76d8934bb6e81bdda352cfb834150502d
이가연 <99721126+dlrkdus@users.noreply.github.com>
2024-12-10 21:18:03 +0900

    feat: util 추가

---
+++ src/main/java/com/hf/healthfriend/domain/notification/util/JsonUtils.java
@@ -0,0 +1,29 @@
+package com.hf.healthfriend.domain.notification.util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hf.healthfriend.domain.notification.dto.NotificationEvent;
+import org.json.JSONObject;
+
+public class JsonUtils {
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    public static String serialize(Object object) {
+        try {
+            return objectMapper.writeValueAsString(object);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("직렬화 실패: " + e.getMessage(), e);
+        }
+    }
+
+    public static NotificationEvent deserializeMessage(String message) {
+        try {
+            JSONObject snsMessage = new JSONObject(message);
+            String messageContent = snsMessage.getString("Message");
+            ObjectMapper objectMapper = new ObjectMapper();
+            return objectMapper.readValue(messageContent, NotificationEvent.class);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("메시지 역직렬화 실패", e);
+        }
+    }
+}
