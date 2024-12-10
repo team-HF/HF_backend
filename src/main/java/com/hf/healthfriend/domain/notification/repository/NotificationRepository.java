7aecec8fd76f777e3b9ab198f5dc9f391c894fe0
이가연 <99721126+dlrkdus@users.noreply.github.com>
2024-12-10 21:13:33 +0900

    feat: 알림 컴포넌트 추가

---
+++ src/main/java/com/hf/healthfriend/domain/notification/repository/NotificationRepository.java
@@ -0,0 +1,7 @@
+package com.hf.healthfriend.domain.notification.repository;
+
+import com.hf.healthfriend.domain.notification.entity.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+}
