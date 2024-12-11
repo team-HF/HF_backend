45e99e16367bffe6e3c99c44383987859f1bd1cc
이가연 <99721126+dlrkdus@users.noreply.github.com>
2024-12-11 13:20:07 +0900

    feat: 에러 코드 추가

--- src/main/java/com/hf/healthfriend/domain/notification/exception/NotificationErrorCode.java
+++ src/main/java/com/hf/healthfriend/domain/notification/exception/NotificationErrorCode.java
@@ -8,7 +8,8 @@ import lombok.Getter;
 public enum NotificationErrorCode {
 
     POST_NOT_FOUND(40401, "NOT001", "존재하지 않는 글에 대한 알림입니다."),
-    COMMENT_NOT_FOUND(40402, "NOT002", "존재하지 않는 댓글에 대한 알림입니다.");
+    COMMENT_NOT_FOUND(40402, "NOT002", "존재하지 않는 댓글에 대한 알림입니다."),
+    MEMBER_NOT_FOUND(40403,"NOT003","존재하지 않는 멤버입니다.");
 
     private final int statusCode;
     private final String errorCode;
