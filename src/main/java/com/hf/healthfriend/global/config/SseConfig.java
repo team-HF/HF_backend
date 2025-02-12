81d7ecf9320889d06503ec54555a88ecc4d727fd
이가연 <99721126+dlrkdus@users.noreply.github.com>
2025-02-12 22:23:10 +0900

    fix: CORS URL 전체 허용

--- src/main/java/com/hf/healthfriend/global/config/SseConfig.java
+++ src/main/java/com/hf/healthfriend/global/config/SseConfig.java
@@ -7,9 +7,9 @@ public class SseConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/hf/connect/sse")
-                .allowedOrigins("http://localhost:3000")  // 클라이언트의 도메인
+                .allowedOrigins("*")  // 클라이언트의 도메인
                 .allowedMethods("GET")  // 허용할 HTTP 메소드
                 .allowedHeaders("Last-Event-ID")  // 허용할 헤더
-                .allowCredentials(true);  // 인증정보 전송 허용
+                .allowCredentials(false);  // 인증정보 전송 허용
     }
 }
