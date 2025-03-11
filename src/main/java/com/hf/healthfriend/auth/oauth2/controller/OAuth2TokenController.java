85218b65e133e95db72a39bc3f5fc5e8e22e46f4
pgd <rudeh1253@gmail.com>
2025-03-11 21:58:53 +0900

    docs: 리프레시 토큰 삭제 엔드포인트에 스웨거 명세

--- src/main/java/com/hf/healthfriend/auth/oauth2/controller/OAuth2TokenController.java
+++ src/main/java/com/hf/healthfriend/auth/oauth2/controller/OAuth2TokenController.java
@@ -13,6 +13,7 @@ import com.hf.healthfriend.global.spec.schema.MemberResponseSchema;
 import com.hf.healthfriend.global.spec.schema.TokenRefreshResponseSchema;
 import com.hf.healthfriend.global.util.HttpCookieUtils;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.headers.Header;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -161,6 +162,20 @@ public class OAuth2TokenController {
     }
 
     @DeleteMapping("/refresh-token")
+    @Operation(
+            summary = "Refresh Token 삭제",
+            description = "Refresh Token을 저장하고 있는 쿠키를 삭제함으로써 Refresh Token 삭제",
+            responses = {
+                    @ApiResponse(
+                            headers = {
+                                    @Header(
+                                            name = HttpHeaders.SET_COOKIE,
+                                            description = "Refresh Token을 담고 있는 쿠키를 삭제하기 위한 헤더"
+                                    )
+                            }
+                    )
+            }
+    )
     public ResponseEntity<Void> deleteRefreshToken() {
         ResponseCookie invalidatorCookie =
                 this.httpCookieUtils.buildCookieInvalidator(CookieConstants.COOKIE_NAME_REFRESH_TOKEN.getString());
