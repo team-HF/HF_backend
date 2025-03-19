821e46575d62c4e4a3b8faa201607e65f683e213
이가연 <99721126+dlrkdus@users.noreply.github.com>
2025-03-19 21:39:21 +0900

    fix: 예외처리 수정

--- src/main/java/com/hf/healthfriend/domain/comment/dto/request/CommentCreationRequestDto.java
+++ src/main/java/com/hf/healthfriend/domain/comment/dto/request/CommentCreationRequestDto.java
@@ -17,9 +17,9 @@ public class CommentCreationRequestDto {
 
     @NotNull
     private Long writerId;
-    
+
     @NotBlank
-    @Size(max = 1000, message = "댓글은 최대 1000자까지 입력할 수 있습니다.")
+    @Size(max = 500, message = "댓글은 최대 500자까지 입력할 수 있습니다.")
     private String content;
 
     @Nullable
