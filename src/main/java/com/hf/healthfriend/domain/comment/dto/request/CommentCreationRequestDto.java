dde2afb783e2f8369a11e2e38392c63d1d1c7273
이가연 <99721126+dlrkdus@users.noreply.github.com>
2025-03-19 21:28:10 +0900

    fix: 댓글 본문 글자수 제한 추가

--- src/main/java/com/hf/healthfriend/domain/comment/dto/request/CommentCreationRequestDto.java
+++ src/main/java/com/hf/healthfriend/domain/comment/dto/request/CommentCreationRequestDto.java
@@ -1,8 +1,10 @@
 package com.hf.healthfriend.domain.comment.dto.request;
 
 import jakarta.annotation.Nullable;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -15,8 +17,9 @@ public class CommentCreationRequestDto {
 
     @NotNull
     private Long writerId;
-
-    @NotEmpty
+    
+    @NotBlank
+    @Size(max = 1000, message = "댓글은 최대 1000자까지 입력할 수 있습니다.")
     private String content;
 
     @Nullable
