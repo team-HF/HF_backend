7912b4518cb4ecacf633f2655d54c78f69535d1b
pgd <rudeh1253@gmail.com>
2025-03-30 13:42:41 +0900

    chore: NGINX 요청 제한

--- dockerize.sh
+++ dockerize.sh
@@ -1,5 +1,5 @@
 BUILD_POSITION=$1
-MODE=$2:-auth
+MODE=$2
 
 # docker container stop hf-backend-app || true
 # docker container rm hf-backend-app || true
@@ -14,7 +14,7 @@ if [ "$BUILD_POSITION" = "no-container" ]; then
   ./gradlew clean build -x test
 fi
 
-docker build -t hf/backend .
+docker build -t rudeh1253/hf-backend:latest .
 
 if [ "$MODE" = "no-auth" ]; then
   docker-compose -f docker-compose.yml --env-file envs --profile blue-noauth up -d --build
