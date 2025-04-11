fc90ce725d7257b8708d64614a91bf42c5114ccb
이가연 <99721126+dlrkdus@users.noreply.github.com>
2025-04-11 21:21:00 +0900

    fix: sns 관련 제거2

--- src/main/java/com/hf/healthfriend/global/config/AWSConfig.java
+++ src/main/java/com/hf/healthfriend/global/config/AWSConfig.java
@@ -1,23 +1,16 @@
 package com.hf.healthfriend.global.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import io.awspring.cloud.sqs.config.SqsMessageListenerContainerFactory;
 import jakarta.annotation.PostConstruct;
-import java.time.Duration;
-import java.util.concurrent.Executor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
-import org.springframework.messaging.converter.MappingJackson2MessageConverter;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 
-import software.amazon.awssdk.services.sns.SnsClient;
 import software.amazon.awssdk.services.sqs.SqsAsyncClient;
-import software.amazon.awssdk.services.sqs.SqsClient;
 
 @Configuration
 public class AWSConfig {
@@ -31,14 +24,6 @@ public class AWSConfig {
     public void checkEnvironmentVariables() {
         System.out.println("AWS_ACCESS_KEY_ID: " + environment.getProperty("AWS_ACCESS_KEY_ID"));
         System.out.println("AWS_SECRET_ACCESS_KEY: " + environment.getProperty("AWS_SECRET_ACCESS_KEY"));
-    }
-
-    @Bean
-    public SnsClient snsClient() {
-        return SnsClient.builder()
-                .region(Region.of(region))
-                .credentialsProvider(DefaultCredentialsProvider.create())
-                .build();
     }
 
     @Bean
